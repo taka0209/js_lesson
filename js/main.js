93d85cda1205df62f21fcb55b74c77d8770b4263
田中崇彬 <taka@tanakatakashiakiranoMacBook-Air.local>
2021-04-06 05:05:32 +0900

    feat: fixed final
    りくさんにOKもらって参考コメントも追加

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 //Lesson1 ulにli要素を追加する
-let list = document.getElementById('list');
-let li = document.createElement('li');
+const list = document.getElementById('list');
+const li = document.createElement('li');
 li.innerHTML = 'これです';
 list.appendChild(li);
+// li.textContent = 'これです';
+// list.appendChild(li);
