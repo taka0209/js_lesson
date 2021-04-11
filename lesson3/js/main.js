8b78fa7c418ec90525137a440dc9de9cd5b0cd91
taka0209 <78735643+taka0209@users.noreply.github.com>
2021-04-11 21:50:48 +0900

    improve: html and js

--- lesson3/js/main.js
+++ lesson3/js/main.js
@@ -13,18 +13,20 @@
    ];
    
    for (let i = 0; i < data.length; i++) {
-      const ul = document.getElementsByTagName('ul');
+      const lists = document.getElementById('lists');
       const li = document.createElement('li');
       const anchor = document.createElement('a');
       const image = document.createElement('img');
       const text = document.createTextNode(data[i].text);
-      anchor.setAttribute('href', data[i].href);
-      image.setAttribute('src', data[i].src);
+      anchor.href = data[i].href;
+      image.src = data[i].src;
+      // anchor.setAttribute('href', data[i].href);
+      // image.setAttribute('src', data[i].src);
       anchor.appendChild(image);
       anchor.appendChild(text);
       image.after(text);
       li.appendChild(anchor);
-      ul[0].appendChild(li);
+      lists.appendChild(li);
    }
    
 
