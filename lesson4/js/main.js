a1751b173a1970161933532e9748a9ba7e6e8eff
taka0209 <78735643+taka0209@users.noreply.github.com>
2021-04-12 05:02:27 +0900

    feat: add js
    jsを追加しました。

---
+++ lesson4/js/main.js
@@ -0,0 +1,38 @@
+'use strict';
+   const data =[
+      {
+         to: "bookmark.html",
+         img: "1.png",
+         alt:"画像1",
+         text: "ブックマーク"
+      }, 
+      {
+         to: "message.html",
+         img: "2.png",
+         alt:"画像2",
+         text: "メッセージ"
+      },
+   ]
+  
+   const fragment = document.createDocumentFragment();
+   const lists = document.getElementById('lists');
+
+   for (let i = 0; i < data.length; i++) {
+      const li = document.createElement('li');
+      const anchor = document.createElement('a');
+      const image = document.createElement('img');
+      const text = document.createTextNode(data[i].text);
+      anchor.href = '/' + data[i].to;
+      image.img = data[i].img;
+      image.alt = data[i].alt;
+      anchor.appendChild(image);
+      anchor.appendChild(text);
+      image.after(text);
+      li.appendChild(anchor);
+      fragment.appendChild(li);
+   }
+
+   lists.appendChild(fragment);
+   
+
+
