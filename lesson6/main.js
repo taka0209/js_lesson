36041cdb208184336dd4c7a12e24dbd5069a9c06
taka0209 <78735643+taka0209@users.noreply.github.com>
2021-06-13 10:55:49 +0900

    feat: create main.js
    課題６を実装しました。

---
+++ lesson6/main.js
@@ -0,0 +1,43 @@
+'use strict';
+   const data =[
+      {
+         to: "bookmark.html",
+         img: "1.png",
+         alt:"画像1",
+         text: "ブックマーク"
+      }, 
+      {
+         to: "message.html",
+         img: "2.png",
+         alt:"画像2",
+         text: "メッセージ"
+      },
+   ]
+   
+   const promise1 = new Promise(function (resolve, reject){
+      resolve (data);
+   });
+   
+   promise1.then(function(data){
+      const fragment = document.createDocumentFragment();
+      const lists = document.getElementById('lists');
+      
+      setTimeout(() => {
+         for (let i = 0; i < data.length; i++) {
+            const li = document.createElement('li');
+            const anchor = document.createElement('a');
+            const image = document.createElement('img');
+            const text = document.createTextNode(data[i].text);
+            anchor.href = '/' + data[i].to;
+            image.img = data[i].img;
+            image.alt = data[i].alt;
+            anchor.appendChild(image);
+            anchor.appendChild(text);
+            li.appendChild(anchor);
+            fragment.appendChild(li);
+         }
+         lists.appendChild(fragment);
+      }, 3000)
+   
+   
+   })
