76b53528c103343196a10a298653aef264099f4b
taka0209 <78735643+taka0209@users.noreply.github.com>
2021-06-19 18:37:41 +0900

    fix: wrapper.removeの位置を変更しました。

--- lesson7/main.js
+++ lesson7/main.js
@@ -1,55 +1,59 @@
-'use strict';
-   //loading iconを表示する
-   const body = document.getElementById('body');
-   const loadingIcon = document.createElement('img');
+//課題の配列
+const data = [
+   {
+      to: "bookmark.html",
+      img: "1.png",
+      alt: "画像1",
+      text: "ブックマーク",
+      src: "img/bookmark.jpeg"
+   },
+   {
+      to: "message.html",
+      img: "2.png",
+      alt: "画像2",
+      text: "メッセージ",
+      src: "img/message.jpeg"
+   }
+];
+
+const body = document.getElementById("body");
+const wrapper = document.createElement("div");
+wrapper.setAttribute("id", "loading-icon");
+body.appendChild(wrapper);
+
+ //loading iconを表示するfunction
+(function () {
+   const loadingIcon = document.createElement("img");
    loadingIcon.src = "img/loading-circle.gif";
-   body.appendChild(loadingIcon);
+   wrapper.appendChild(loadingIcon);
+ })(); 
+ 
+ //promiseで解決された値とする
+const menuList = new Promise(function (resolve, reject) {
+   //3秒待機
+   setTimeout(() => {
+      resolve(data);
+   }, 3000);
+});
 
-   //課題の配列
-   const data =[
-      {
-         to: "bookmark.html",
-         img: "1.png",
-         alt:"画像1",
-         text: "ブックマーク",
-         src: "img/bookmark.jpeg"
-      }, 
-      {
-         to: "message.html",
-         img: "2.png",
-         alt:"画像2",
-         text: "メッセージ",
-         src: "img/message.jpeg"
-      },
-   ]
-
-   //promiseで解決された値とする
-   const menuList = new Promise(function (resolve, reject){
-      //3秒待機
-      setTimeout(() => {
-         resolve (data);
-      }, 3000)
+ //解決された値を受ける
+menuList.then(function (data) {
+   wrapper.remove(); //ローディングアイコンを非表示にする
+   const fragment = document.createDocumentFragment();
+   const lists = document.getElementById("lists");
+   data.forEach((value) => {
+      const li = document.createElement("li");
+      const anchor = document.createElement("a");
+      const image = document.createElement("img");
+      const text = document.createTextNode(value.text);
+      anchor.href = `/${value.to}`;
+      image.alt = value.alt;
+      image.src = value.src;
+      anchor.appendChild(text);
+      anchor.appendChild(image);
+      li.appendChild(anchor);
+      fragment.appendChild(li);
    });
-   
-   //解決された値を受ける
-   menuList.then(function(data){
-      const fragment = document.createDocumentFragment();
-      const lists = document.getElementById('lists');
-         data.forEach(value => {
-            const li = document.createElement('li');
-            const anchor = document.createElement('a');
-            const image = document.createElement('img');
-            const text = document.createTextNode(value.text);
-            anchor.href = `/${value.to}`;
-            image.alt = value.alt;
-            image.src = value.src;
-            anchor.appendChild(text);
-            anchor.appendChild(image);
-            li.appendChild(anchor);
-            fragment.appendChild(li);
-            loadingIcon.remove();
-         });
-      
-         lists.appendChild(fragment);
-   })
 
+   lists.appendChild(fragment);
+});
