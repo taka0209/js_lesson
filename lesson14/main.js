a1344c62feb35149e71517b562550313aa5e39e4
taka0209 <78735643+taka0209@users.noreply.github.com>
2021-12-13 21:31:34 +0900

    fix: Changed the declaration position of constants

--- lesson14/main.js
+++ lesson14/main.js
@@ -1,35 +1,38 @@
-'use strict';
+"use strict";
 const wrapper = document.getElementById("wrapper");
+const lists = document.getElementById("lists");
 const createElBtn = document.getElementById("create-element-btn");
 createElBtn.classList.add("btn");
 const modal = document.getElementById("modal");
 const displayModal = document.getElementById("display-modal");
 const jsonURL = "https://myjson.dit.upm.es/api/bins/j16z";
 const removeModal = () => {
-   modal.remove();
-   body.classList.remove("bg_gray");
-}
+  modal.remove();
+  wrapper.classList.remove("bg_gray");
+};
+
+const loadingIcon = document.createElement("img");
 const startLoading = () => {
-   const loadingIcon = document.createElement("img");
    loadingIcon.id = "loadingIcon";
    loadingIcon.src = "img/loading-circle.gif";
    wrapper.appendChild(loadingIcon);
 };
+
 const endLoading = () => loadingIcon.remove();
 
-displayModal.addEventListener('click', function() {
-   modal.style.display = 'block';
-   body.classList.add("bg_gray");
+displayModal.addEventListener("click", function () {
+   modal.style.display = "block";
+   wrapper.classList.add("bg_gray");
    displayModal.remove();
 });
 
-createElBtn.addEventListener('click', () => {
+createElBtn.addEventListener("click", () => {
    const inputNumber = document.getElementById("number");
-   if(!inputNumber.value === false){
+   if (!inputNumber.value === false) {
       console.log(inputNumber.value);
       init();
    } else {
-      alert('数字が未入力です。');
+      alert("数字が未入力です。");
    }
 });
 
@@ -40,31 +43,30 @@ async function init() {
    try {
       const result = await fetchData();
       createListElements(result);
-   } catch(e) {
+   } catch (e) {
       const div = document.createElement("div");
       lists.appendChild(div);
-      div.textContent =` エラーが発生しました：${e}`;
+      div.textContent = ` エラーが発生しました：${e}`;
       console.log(e);
    } finally {
       endLoading();
       displayModal.remove();
    }
-};
+}
 
 async function fetchData() {
    try {
       const response = await fetch(jsonURL);
       const json = await response.json();
       return json.data;
-   } catch(error) {
+   } catch (error) {
       throw new Error(error);
    }
-};
+}
 
 const createListElements = (data) => {
    const fragment = document.createDocumentFragment();
-   const lists = document.getElementById("lists");
-   for (let i = 0; i < data.length; i++){
+   for (let i = 0; i < data.length; i++) {
       const li = document.createElement("li");
       const anchor = document.createElement("a");
       const image = document.createElement("img");
