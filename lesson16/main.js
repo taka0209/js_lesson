8bbd5acb6d57133ea5cd5164af932a12a9fef2c1
taka0209 <78735643+taka0209@users.noreply.github.com>
2022-02-28 04:50:25 +0900

    fix: separated the articles creation part by function

--- lesson16/main.js
+++ lesson16/main.js
@@ -1,7 +1,6 @@
 "use strict";
 const body = document.getElementById("body");
 let categoryTab = document.getElementById("js-categoryTab");
-
 const jsonURL = "http://myjson.dit.upm.es/api/bins/du3p";
 // const jsonURL = "https://myjson.dit.upm.es/api/bins/gdod";　データが空のjson
 async function fetchData(url) {
@@ -9,26 +8,22 @@ async function fetchData(url) {
    const json = await response.json();
    return json.data;
 }
-
 const createElementWithClassName = (element, name) => {
    const createElement = document.createElement(element);
    createElement.classList.add(name);
    return createElement;
 };
-
 const displayMessage = (message) => {
    const div = document.createElement("div");
    body.appendChild(div);
    div.textContent = message;
 }
-
 const startLoading = () => {
    const loadingIcon = document.createElement("img");
    loadingIcon.id = "loadingIcon";
    loadingIcon.src = "img/loading-circle.gif";
    body.appendChild(loadingIcon);
 };
-
 const endLoading = () => {
    const loadingIcon = document.getElementById("loadingIcon");
    loadingIcon.remove();
@@ -40,7 +35,6 @@ async function createTabContents() {
    let responseData;
    try {
       responseData = await fetchData(jsonURL);
-      console.log(responseData);
       if(responseData.length === 0) {
          displayMessage("データはまだありません");
          return;
@@ -54,7 +48,6 @@ async function createTabContents() {
    createCategoryTab(responseData);
    renderTabContainer(responseData);
 }
-
 const createCategoryTab = (values) => {
    const fragment = document.createDocumentFragment();
    for (let i = 0; i < values.length; i++) {
@@ -74,25 +67,16 @@ const renderTabContainer = (values) => {
    const tabFragment = document.createDocumentFragment();
    const tabContainer = createElementWithClassName("div", "tab-container");
    tabContainer.id = "js-tabContainer";
-   const articlesData = values.map((value) => value.articles);
    for (let i = 0; i < values.length; i++) {
       const tabContents = createElementWithClassName("div", "tab-contents");
       tabContents.id = `js-tabContents${i}`;
-      const articlesTitles = articlesData[i].map((value) => value.title);
-      const articlesHref = articlesData[i].map((value) => value.href);
       const tabLists = createElementWithClassName("ul", "tab-contents__lists");
       tabLists.id = `js-tabLists${i}`;
       const tabImage = createElementWithClassName("img", "tab-contents__image");
       tabImage.id = `js-tabImage${i}`;
       tabImage.src = values[i].img;
-      tabContents.appendChild(tabImage);
-      for (let j = 0; j < articlesTitles.length; j++) {
-         const tabItems = createElementWithClassName("li", "lists__item");
-         const tabItemsHref = createElementWithClassName("a", "lists__item__text");
-         tabItemsHref.href = articlesHref[j];
-         tabItemsHref.textContent = articlesTitles[j];
-         tabFragment.appendChild(tabContainer).appendChild(tabContents).appendChild(tabLists).appendChild(tabItems).appendChild(tabItemsHref);
-      }
+      tabContents.appendChild(tabLists).appendChild(createArticlesTitles(values[i]));
+      tabFragment.appendChild(tabContainer).appendChild(tabContents).appendChild(tabImage);
    }
    body.insertBefore(tabFragment, categoryTab.nextElementSibling);
    //TODO:どのカテゴリタブを初期表示時に選んでいるかはデータとして持っている
@@ -101,20 +85,33 @@ const renderTabContainer = (values) => {
    newsContents.classList.add("is-show");
 }
 
-categoryTab.addEventListener("click", (e) => {
-   const activeTab = document.querySelector(".is-active");
-   const showContents = document.querySelector(".is-show");
-   activeTab.classList.remove("is-active");
-   e.target.classList.add("is-active");
-   const arrayCategoryTab = [...document.querySelectorAll(".category-tab__item")]
-   const isActive = document.getElementsByClassName("is-active")[0];
-   const index = arrayCategoryTab.indexOf(isActive);
-   showContents.classList.remove("is-show");
-   const tabContents = document.getElementsByClassName("tab-contents");
-   tabContents[index].classList.add("is-show");
-});
+const createArticlesTitles = (values) => {
+   const articlesTitlesFragment = document.createDocumentFragment();
+   for (let i = 0; i < values.articles.length; i++) {
+      const articlesHref = values.articles.map((value) => value.href);
+      const articlesTitles = values.articles.map((value) => value.title);
+      const tabItems = createElementWithClassName("li", "lists__item");
+      const tabItemsHref = createElementWithClassName("a", "lists__item__text");
+      tabItems.textContent = articlesTitles[i];
+      tabItemsHref.href = articlesHref[i];
+      articlesTitlesFragment.appendChild(tabItems).appendChild(tabItemsHref);
+   }
+   return articlesTitlesFragment;
+}
 
-//以下は、現段階では未実装
-// 記事にはnewという新着かどうかのラベルがつく(どこの記事にそれが入るかは適当でいいです)
-// 記事にはそれぞれコメントがあり、0件なら表示しない、1以上ならアイコンと共に数字が表示される
+   categoryTab.addEventListener("click", (e) => {
+      const activeTab = document.querySelector(".is-active");
+      const showContents = document.querySelector(".is-show");
+      activeTab.classList.remove("is-active");
+      e.target.classList.add("is-active");
+      const arrayCategoryTab = [...document.querySelectorAll(".category-tab__item")]
+      const isActive = document.getElementsByClassName("is-active")[0];
+      const index = arrayCategoryTab.indexOf(isActive);
+      showContents.classList.remove("is-show");
+      const tabContents = document.getElementsByClassName("tab-contents");
+      tabContents[index].classList.add("is-show");
+   });
+   //以下は、現段階では未実装
+   // 記事にはnewという新着かどうかのラベルがつく(どこの記事にそれが入るかは適当でいいです)
+   // 記事にはそれぞれコメントがあり、0件なら表示しない、1以上ならアイコンと共に数字が表示される
 // どのカテゴリタブを初期表示時に選んでいるかはデータとして持っている
